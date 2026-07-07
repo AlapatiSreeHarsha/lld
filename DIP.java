4bae8147c2e6e64b253686f2f0f3d5d16f2694e5
AlapatiSreeHarsha <alapatisreeharsha139@gmail.com>
2026-07-07 11:15:40 +0530

    DIP

---
+++ DIP.java
@@ -0,0 +1,50 @@
+//Depend on interfaces (abstractions), not on concrete classes.
+interface Recommendation{
+    void recommend();
+}
+class TrendingRecommonation implements Recommendation{
+    public void recommend(){
+        System.out.println("Showing trending recommendations");
+    }
+}
+class RecentReccommendation implements Recommendation{
+    public void recommend(){
+        System.out.println("Showing recent recommendations");
+    }
+}
+
+class GenreRecommonation implements Recommendation{
+    public void recommend(){
+        System.out.println("Showing genre-specific recommendations");
+    }
+}
+
+class RecommendationService{
+    private Recommendation recommendation;
+    public RecommendationService(Recommendation recommendation){
+        this.recommendation=recommendation;
+    }
+    public void showRecommendation(){
+        recommendation.recommend();
+    }
+}
+
+
+
+public class DIP { //Netflix class
+    public static void main(String[] args){
+        
+        Recommendation trending=new TrendingRecommonation();
+        Recommendation recent=new RecentReccommendation();
+        Recommendation genre=new GenreRecommonation();
+
+        RecommendationService service=new RecommendationService(trending);
+        service.showRecommendation();
+
+        service=new RecommendationService(recent);
+        service.showRecommendation();
+
+        service=new RecommendationService(genre);
+        service.showRecommendation();
+    }
+}
